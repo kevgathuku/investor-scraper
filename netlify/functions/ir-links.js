6ea134a4451e1b361f3a5e9926e5ec1a92854ae1
Kevin Gathuku <kevgathuku@gmail.com>
2022-02-03 12:28:11 +0300

    Add IR-links netlify function

---
+++ netlify/functions/ir-links.js
@@ -0,0 +1,46 @@
+const puppeteer = require("puppeteer");
+
+exports.handler = async function (event) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const symbol = event.queryStringParameters.symbol;
+
+  //   Execute Google search for the provided company
+  await page.goto(`https://google.com/search?q=${symbol} investor relations`);
+
+  await page.screenshot({ path: "tmp/result.png" });
+
+  const resultsSelector = ".g a";
+
+  const links = await page.evaluate((resultsSelector) => {
+    const anchors = Array.from(document.querySelectorAll(resultsSelector));
+
+    console.log("anchors", anchors);
+
+    return anchors
+      .map((anchor) => {
+        const link = anchor.href;
+        return link;
+      })
+      .filter(
+        (link) =>
+          !link.startsWith("https://www.google.com/search?q") &&
+          !link.startsWith("https://translate.google.com/translate?hl") &&
+          !link.startsWith("https://webcache.googleusercontent.com/search?q")
+      );
+  }, resultsSelector);
+
+  console.log("Links count", links.length);
+  console.log("First result", links[0]);
+
+  await browser.close();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      top_result: links[0],
+      all_results: links,
+    }),
+  };
+};
